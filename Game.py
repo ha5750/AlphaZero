cb78eafd8e75d26a3b39110924d20879911ca3b0
ha5750 <ckim135@jhu.edu>
2022-05-17 19:00:23 -0400

    update game

--- Game.py
+++ Game.py
@@ -1,246 +1,219 @@
+
+from __future__ import print_function
 import numpy as np
 
 
-"""
-Defines Go Board and Game classes
-"""
-
 class Board(object):
-    '''
-    Game Board
-    '''
+    """board for the game"""
 
-    def __init__(self, n):
-        '''
-        initialize board with n x n board
-        states: dictionary with key = move, value = player
-        move is a natural number defined as follows if n = 3
-        7 8 9
-        4 5 6
-        1 2 3
-        '''
-        self.n = n 
-        self.n_in_row = 5
+    def __init__(self, **kwargs):
+        self.width = int(kwargs.get('width', 8))
+        self.height = int(kwargs.get('height', 8))
+        # board states stored as a dict,
+        # key: move as location on the board,
+        # value: player as pieces type
         self.states = {}
-        self.players = [1, 2]
-    
+        # need how many pieces in a row to win
+        self.n_in_row = int(kwargs.get('n_in_row', 5))
+        self.players = [1, 2]  # player1 and player2
 
-    def init_board(self, start_player = 0):
-        '''
-        initializes current_player, available moves, and states
-        start_player: index 0 or 1 to denote players 1 and 2
-        avals: list of possible moves (moves that have not been played)
-        last_move: last move played on this board
-        '''
-        self.cur_player = self.players[start_player]
-        self.avals = set(range(self.n * self.n))
+    def init_board(self, start_player=0):
+        if self.width < self.n_in_row or self.height < self.n_in_row:
+            raise Exception('board width and height can not be '
+                            'less than {}'.format(self.n_in_row))
+        self.current_player = self.players[start_player]  # start player
+        # keep available moves in a list
+        self.availables = list(range(self.width * self.height))
         self.states = {}
         self.last_move = -1
-    
-    def move_to_coord(self, move):
-        '''
-        converts move into coordinate format [h, w]
-        7 8 9       (2,0) (2,1) (2,2)
-        4 5 6  -->  (1,0) (1,1) (1,2)
-        1 2 3       (0,0) (0,1) (0,2) 
-        '''
-        return [move // self.n, move % self.n]
 
-    
-    def coord_to_move(self, coord):
-        '''
-        converts coordinate format to move
-        '''
-        return coord[0] * self.n + coord[1]
+    def move_to_location(self, move):
+        """
+        3*3 board's moves like:
+        6 7 8
+        3 4 5
+        0 1 2
+        and move 5's location is (1,2)
+        """
+        h = move // self.width
+        w = move % self.width
+        return [h, w]
 
+    def location_to_move(self, location):
+        if len(location) != 2:
+            return -1
+        h = location[0]
+        w = location[1]
+        move = h * self.width + w
+        if move not in range(self.width * self.height):
+            return -1
+        return move
 
-    def make_move(self, move):
-        '''
-        makes a move 
-        '''
-        self.states[move] = self.cur_player
-        self.avals.remove(move)
-        self.cur_player = (
-            1 if self.cur_player == 2
-            else 2
+    def current_state(self):
+        """return the board state from the perspective of the current player.
+        state shape: 4*width*height
+        """
+
+        square_state = np.zeros((4, self.width, self.height))
+        if self.states:
+            moves, players = np.array(list(zip(*self.states.items())))
+            move_curr = moves[players == self.current_player]
+            move_oppo = moves[players != self.current_player]
+            square_state[0][move_curr // self.width,
+                            move_curr % self.height] = 1.0
+            square_state[1][move_oppo // self.width,
+                            move_oppo % self.height] = 1.0
+            # indicate the last move location
+            square_state[2][self.last_move // self.width,
+                            self.last_move % self.height] = 1.0
+        if len(self.states) % 2 == 0:
+            square_state[3][:, :] = 1.0  # indicate the colour to play
+        return square_state[:, ::-1, :]
+
+    def do_move(self, move):
+        self.states[move] = self.current_player
+        self.availables.remove(move)
+        self.current_player = (
+            self.players[0] if self.current_player == self.players[1]
+            else self.players[1]
         )
         self.last_move = move
 
-    def has_winner(self):
-        '''
-        returns [True, player] if game won
-        returns [False, -1] if no one won 
-        '''
+    def has_a_winner(self):
+        width = self.width
+        height = self.height
         states = self.states
-        moved = list(set(range(self.n*self.n)) - self.avals) #list of made moves
-        if len(moved) < 9:
+        n = self.n_in_row
+
+        moved = list(set(range(width * height)) - set(self.availables))
+        if len(moved) < self.n_in_row *2-1:
             return False, -1
-        
+
         for m in moved:
-            h, w = self.move_to_coord(m)
+            h = m // width
+            w = m % width
             player = states[m]
 
-            if (w in range(self.n - 5 + 1) and
-                    len(set(states.get(i, -1) for i in range(m, m + 5))) == 1):
+            if (w in range(width - n + 1) and
+                    len(set(states.get(i, -1) for i in range(m, m + n))) == 1):
                 return True, player
 
-            if (h in range(self.n - 5 + 1) and
-                    len(set(states.get(i, -1) for i in range(m, m + 5 * self.n, self.n))) == 1):
+            if (h in range(height - n + 1) and
+                    len(set(states.get(i, -1) for i in range(m, m + n * width, width))) == 1):
                 return True, player
 
-            if (w in range(self.n - 5 + 1) and h in range(self.n - 5 + 1) and
-                    len(set(states.get(i, -1) for i in range(m, m + 5 * (self.n + 1), self.n + 1))) == 1):
+            if (w in range(width - n + 1) and h in range(height - n + 1) and
+                    len(set(states.get(i, -1) for i in range(m, m + n * (width + 1), width + 1))) == 1):
                 return True, player
 
-            if (w in range(5 - 1, self.n) and h in range(self.n - 5 + 1) and
-                    len(set(states.get(i, -1) for i in range(m, m + 5 * (self.n - 1), self.n - 1))) == 1):
+            if (w in range(n - 1, width) and h in range(height - n + 1) and
+                    len(set(states.get(i, -1) for i in range(m, m + n * (width - 1), width - 1))) == 1):
                 return True, player
-    
+
         return False, -1
-    
-    def game_done(self):
-        '''
-        check if the game is done
-        '''
-        if (len(self.avals) == 0): #draw
-            return True, -1
-        return self.has_winner()
-    
-    def get_cur_player(self):
-        '''
-        returns current player (1 or 2)
-        '''
-        return self.cur_player
 
-    '''
-    returns board state where its shape is (4,n,n) from the cur_player's pespective
-    Each of the 4 elements contains a (nxn) board where
-    state[0]: cur_players' moves marked by 1.0 on the board
-    state[1]: opponenent's moves marked by 1.0 on the board
-    state[2]: last move's location marked by 1.0 on the board
-    state[3]: all 1 if both players went, all 0 otherwise
-    '''
-    def cur_state(self):
-        state = np.zeros((4, self.n, self.n))
-        if self.states:
-            moves, players = np.array(list(zip(*self.states.items())))
-            curr_moves = moves[players == self.cur_player]
-            oppo_moves = moves[players != self.cur_player]
-            state[0][curr_moves // self.n, curr_moves % self.n] = 1.0
-            state[1][oppo_moves // self.n, oppo_moves % self.n] = 1.0
-            state[2][self.last_move // self.n, self.last_move % self.n] = 1.0
-        
-        if len(self.states) % 2 == 0:
-            state[3][:, :] = 1.0
-        
-        return state[:, ::-1, :] #flip board vertically for visual
+    def game_end(self):
+        """Check whether the game is ended or not"""
+        win, winner = self.has_a_winner()
+        if win:
+            return True, winner
+        elif not len(self.availables):
+            return True, -1
+        return False, -1
+
+    def get_current_player(self):
+        return self.current_player
 
 
 class Game(object):
-    '''
-    Game Server
-    '''
+    """game server"""
 
-    def __init__(self, board):
+    def __init__(self, board, **kwargs):
         self.board = board
-    
-    def display(self, board, player1, player2):
-        '''
-        Displays the board with game info
-        '''
+
+    def graphic(self, board, player1, player2):
+        """Draw the board and show game info"""
+        width = board.width
+        height = board.height
+
         print("Player", player1, "with X".rjust(3))
         print("Player", player2, "with O".rjust(3))
         print()
-        for x in range(board.n):
+        for x in range(width):
             print("{0:8}".format(x), end='')
         print('\r\n')
-        for i in range(board.n - 1, -1, -1):
+        for i in range(height - 1, -1, -1):
             print("{0:4d}".format(i), end='')
-            for j in range(board.n):
-                move = i * board.n + j
-                player = board.states.get(move, -1)
-                if (player == -1):
-                    print('_'.center(8), end='')
-                elif (player == player1):
+            for j in range(width):
+                loc = i * width + j
+                p = board.states.get(loc, -1)
+                if p == player1:
                     print('X'.center(8), end='')
-                elif (player == player2):
+                elif p == player2:
                     print('O'.center(8), end='')
+                else:
+                    print('_'.center(8), end='')
             print('\r\n\r\n')
-    
-    def start_play(self, player1, player2, start_player = 0, show_board = 1):
-        '''
-        starts game with two players
-        '''
+
+    def start_play(self, player1, player2, start_player=0, is_shown=1):
+        """start a game between two players"""
+        if start_player not in (0, 1):
+            raise Exception('start_player should be either 0 (player1 first) '
+                            'or 1 (player2 first)')
         self.board.init_board(start_player)
         p1, p2 = self.board.players
         player1.set_player_ind(p1)
         player2.set_player_ind(p2)
         players = {p1: player1, p2: player2}
-        if show_board:
-            self.display(self.board, player1.player, player2.player)
+        if is_shown:
+            self.graphic(self.board, player1.player, player2.player)
         while True:
-            cur_player = self.board.get_cur_player()
-            player_in_turn = players[cur_player]
+            current_player = self.board.get_current_player()
+            player_in_turn = players[current_player]
             move = player_in_turn.get_action(self.board)
             self.board.do_move(move)
-            if show_board:
-                self.displaY(self.board, player1.player, player2.player)
-            end, winner = self.board.game_done()
+            if is_shown:
+                self.graphic(self.board, player1.player, player2.player)
+            end, winner = self.board.game_end()
             if end:
-                if show_board:
+                if is_shown:
                     if winner != -1:
                         print("Game end. Winner is", players[winner])
                     else:
                         print("Game end. Tie")
                 return winner
 
-    def start_self_play(self, player, show_board=0, temp=1e-3):
-        '''
-        starts a self-play game using MCTS, and store the self-play data
-        (state, mcts_probs, z) for training
-        '''
+    def start_self_play(self, player, is_shown=0, temp=1e-3):
+        """ start a self-play game using a MCTS player, reuse the search tree,
+        and store the self-play data: (state, mcts_probs, z) for training
+        """
         self.board.init_board()
         p1, p2 = self.board.players
         states, mcts_probs, current_players = [], [], []
         while True:
-            move, move_probs = player.get_action(self.board, temp=temp,
-                                                return_prob = 1)
-            #store data
-            states.append(self.board.cur_state())
+            move, move_probs = player.get_action(self.board,
+                                                 temp=temp,
+                                                 return_prob=1)
+            # store the data
+            states.append(self.board.current_state())
             mcts_probs.append(move_probs)
-            current_players.append(self.board.current_players)
-            #perform a move
+            current_players.append(self.board.current_player)
+            # perform a move
             self.board.do_move(move)
-            if show_board:
-                self.display(self.board, p1, p2)
+            if is_shown:
+                self.graphic(self.board, p1, p2)
             end, winner = self.board.game_end()
             if end:
+                # winner from the perspective of the current player of each state
                 winners_z = np.zeros(len(current_players))
                 if winner != -1:
                     winners_z[np.array(current_players) == winner] = 1.0
                     winners_z[np.array(current_players) != winner] = -1.0
-                #reset MCTS root node
-                if show_board:
+                # reset MCTS root node
+                player.reset_player()
+                if is_shown:
                     if winner != -1:
-                        print("Game end. WInner is player:", winner)
+                        print("Game end. Winner is player:", winner)
                     else:
-                        print("Game end. Draw")
+                        print("Game end. Tie")
                 return winner, zip(states, mcts_probs, winners_z)
-
-
-
-
-
-
-    
-
-
-
-
-
-
-
-
-        
-
-        
