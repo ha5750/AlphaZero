1e2040e4e45312c3e84cbdc7f03c1e2fd81854dc
ha5750 <ckim135@jhu.edu>
2022-05-17 20:13:12 -0400

    update humanAIplay

--- humanAI_play.py
+++ humanAI_play.py
@@ -11,6 +11,7 @@ from mcts import MCTSPlayer as MCTS_Pure
 from mcts_alphaZero import MCTSPlayer
 from policy_value_network import PolicyValueNet 
 import tensorflow as tf
+import argparse
 
 class Human(object):
     """
@@ -31,7 +32,7 @@ class Human(object):
             move = board.location_to_move(location)
         except Exception as e:
             move = -1
-        if move == -1 or move not in board.avals:
+        if move == -1 or move not in board.availables:
             print("invalid move")
             move = self.get_action(board)
         return move
@@ -40,27 +41,30 @@ class Human(object):
         return "Human {}".format(self.player)
 
 
-def run():
+def run(model_name, start_player):
     n = 5
     width, height = 15, 15
-    model_file = 'best_policy.model'
+    
     try:
         board = Board(width=width, height=height, n_in_row=n)
         game = Game(board)
-
-        best_policy = PolicyValueNet(width, height, model_file = model_file)
+        best_policy = PolicyValueNet(width, height, model_file = model_name)
         mcts_player = MCTSPlayer(best_policy.policy_value_fn, c_puct=5, n_playout=1000)
 
         # human player, input your move in the format: 2,3
         human = Human()
 
         # set start_player=0 for human first
-        game.start_play(human, mcts_player, start_player=1, is_shown=1)
+        game.start_play(human, mcts_player, start_player=start_player, is_shown=1)
     except KeyboardInterrupt:
         print('\n\rquit')
 
 
 if __name__ == '__main__':
+    # parse the commandline
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', help = "initial model if needed e.g. (number).model")
+    parser.add_argument('--start', help = "0: Human goes first, 1: AI goes first")
+    args = parser.parse_args()
     tf.compat.v1.disable_eager_execution()
-
-    run()
+    run(args.model, int(args.start))
